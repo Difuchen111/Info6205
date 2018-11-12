2c0b075a6715963eaf1a48f170b11d00c8e6bbdd
Difuchen111 <44352568+Difuchen111@users.noreply.github.com>
2018-11-12 17:55:38 -0500

    Add files via upload

---
+++ Difu_Chen_Assignment_5/Assignment5/src/main/java/edu/neu/coe/info6205/bqs/QueueTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2017. Phasmid Software
+ */
+
+package main.java.edu.neu.coe.info6205.bqs;
+
+import org.junit.Test;
+
+import main.java.edu.neu.coe.info6205.bqs.Queue_Elements;
+
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+public class QueueTest {
+
+    @Test
+    public void testQueue0() {
+        Queue<Integer> queue = new Queue_Elements<>();
+        assertTrue(queue.isEmpty());
+        assertNull(queue.dequeue());
+        assertTrue(queue.isEmpty());
+    }
+
+    @Test
+    public void testQueue1() {
+        Queue<Integer> queue = new Queue_Elements<>();
+        assertTrue(queue.isEmpty());
+        queue.enqueue(1);
+        assertFalse(queue.isEmpty());
+        assertEquals(queue.dequeue(), new Integer(1));
+        assertTrue(queue.isEmpty());
+    }
+
+    @Test
+    public void testQueue2() {
+        Queue<Integer> queue = new Queue_Elements<>();
+        assertTrue(queue.isEmpty());
+        queue.enqueue(1);
+        queue.enqueue(2);
+        assertFalse(queue.isEmpty());
+        assertEquals(queue.dequeue(), new Integer(1));
+        assertEquals(queue.dequeue(), new Integer(2));
+        assertTrue(queue.isEmpty());
+    }
+
+    @Test
+    public void testQueue3() {
+        Queue<Integer> queue = new Queue_Elements<>();
+        assertTrue(queue.isEmpty());
+        queue.enqueue(1);
+        queue.enqueue(2);
+        assertFalse(queue.isEmpty());
+        assertEquals(queue.dequeue(), new Integer(1));
+        queue.enqueue(3);
+        queue.enqueue(4);
+        assertEquals(queue.dequeue(), new Integer(2));
+        assertEquals(queue.dequeue(), new Integer(3));
+        assertEquals(queue.dequeue(), new Integer(4));
+        assertTrue(queue.isEmpty());
+    }
+}
